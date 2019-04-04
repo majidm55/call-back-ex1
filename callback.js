afb369418b4d25a97a81c70e5f27e1368ff5486e
MAJID <mohammadmajid55@gmail.com>
2019-04-04 16:43:42 +0000

    finished ex1

---
+++ callback.js
@@ -0,0 +1,15 @@
+function findWaldo(arr, found) {
+  // for (var i = 0; i < arr.length; i++) {
+    arr.forEach(function(name,index) {
+    if (name === "Waldo") {
+    found (index)   // execute callback
+    };
+})
+}
+
+function actionWhenFound(index) {
+
+  console.log("Found Waldo at index " + index + "!" );
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
